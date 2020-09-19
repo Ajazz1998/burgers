a6968539afaa2322c637b6830803a70ded4cfa5f
Ajay Bava <ajaybava1998@gmail.com>
2020-09-19 15:29:15 +0100

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('opens').addEventListener('click', () => {
+    const nav = document.getElementById('nav');
+
+    if(nav.style.display === 'none'){
+        nav.style.display = 'block'
+    } else {
+        nav.style.display = 'none'
+    }
+})
